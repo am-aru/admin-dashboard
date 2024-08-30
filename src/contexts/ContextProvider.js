2e81ddc9eb22ce30a6137fa9a374b926d7331752
Arpita Sharma <arpita.arusharma@gmail.com>
2024-08-30 22:25:11 +0530

    adding functionality to cross button in sidebar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,7 +13,7 @@ export const ContextProvider = ({ children }) =>{
 
     return (
         <StateContext.Provider 
-        value={{ activeMenu, }}>
+        value={{ activeMenu, setActiveMenu }}>
             { children }
         </StateContext.Provider>
     )
